26699e1f8c717632f64d1bc16f2d72c9d7ffdf18
HYDRA242 <ranggarifai1945@gmail.com>
2022-09-20 13:13:03 -0700

    nomor 1

---
+++ database/seeders/User.php
@@ -0,0 +1,25 @@
+<?php
+ 
+namespace Database\Seeders;
+ 
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+ 
+class FirstSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=> 'rangga',
+            'email' => 'rangga@gmail.com',
+            'password'=> bcrypt('password'),
+        ]);
+ 
+    }
+}
